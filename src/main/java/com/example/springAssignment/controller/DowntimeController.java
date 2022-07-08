58d7da1d232d6e3a79c3a483de608b6e8e52fcaf
rhythmgupta <rhythm18082@iiitd.ac.in>
2022-07-08 21:23:35 +0530

    added put, post, delete methods

--- src/main/java/com/example/springAssignment/controller/DowntimeController.java
+++ src/main/java/com/example/springAssignment/controller/DowntimeController.java
@@ -2,8 +2,13 @@ package com.example.springAssignment.controller;
 
 import com.example.springAssignment.model.Downtime;
 import com.example.springAssignment.repository.DowntimeRepository;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Optional;
 
 @RestController
 @RequestMapping
@@ -20,17 +25,41 @@ public class DowntimeController {
         return ResponseEntity.ok(this.downtimeRepository.findAll());
     }
 
+    @GetMapping("/getbyid")
+    public ResponseEntity getDowntime(@RequestParam long id){
+        Optional<Downtime> downtimes = this.downtimeRepository.findById(id);
+        if (!downtimes.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(this.downtimeRepository.findById(id));
+    }
+
     @PostMapping("/post")
     public ResponseEntity postDowntime(@RequestParam(name="provider") String provider, @RequestParam(name = "flow_name") String flow_name, @RequestBody Downtime p){
         p.setFlow(flow_name);
         p.setProvider(provider);
-        return ResponseEntity.ok(this.downtimeRepository.save(p));
+        Downtime d = this.downtimeRepository.save(p);
+        URI loc = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(d.getId()).toUri();
+        return ResponseEntity.created(loc).build();
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity deleteDowntime(@RequestParam long id){
+    public ResponseEntity deleteDowntime(@RequestParam(name = "id") Long id){
         this.downtimeRepository.deleteById(id);
-        return ResponseEntity.ok("Deleted");
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/put")
+    public ResponseEntity modifyDowntime(@RequestParam(name = "id") Long id, @RequestParam(name="provider") String provider, @RequestParam(name = "flow_name") String flow_name, @RequestBody Downtime p){
+        Optional<Downtime> downtimes = this.downtimeRepository.findById(id);
+        if (!downtimes.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+        p.setId(id);
+        p.setFlow(flow_name);
+        p.setProvider(provider);
+        this.downtimeRepository.save(p);
+        return ResponseEntity.noContent().build();
     }
 
 }
